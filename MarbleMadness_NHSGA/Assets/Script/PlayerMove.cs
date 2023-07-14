1fb6ae890e2d68ac2ad07d6998f5b508d4e23092
Michael-1898 <85533503+Michael-1898@users.noreply.github.com>
2023-07-14 13:48:42 -0400

    playerMovement basic

--- MarbleMadness_NHSGA/Assets/Script/PlayerMove.cs
+++ MarbleMadness_NHSGA/Assets/Script/PlayerMove.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMove : MonoBehaviour
 {
+    [SerializeField] Rigidbody rb;
+    private Vector3 moveRawInput;
+    private Vector3 moveDirection;
+    [SerializeField] float moveForce;
+    [SerializeField] Camera cam;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,17 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        moveDirection = cam.transform.TransformDirection(moveRawInput);
+        moveDirection = new Vector3(moveDirection.x, 0, moveDirection.z);
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(moveDirection * moveForce, ForceMode.Impulse);
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveRawInput = new Vector3(value.Get<Vector2>().x, 0, value.Get<Vector2>().y);
     }
 }
