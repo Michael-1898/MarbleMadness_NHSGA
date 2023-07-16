20a24bd2f4badd9f90740f9f004f821faea6ef92
Michael-1898 <85533503+Michael-1898@users.noreply.github.com>
2023-07-16 02:30:15 -0400

    base enemy marble movement

--- MarbleMadness_NHSGA/Assets/Scripts/Enemies/BallEnemy/BallMovement.cs
+++ MarbleMadness_NHSGA/Assets/Scripts/Enemies/BallEnemy/BallMovement.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class BallMovement : MonoBehaviour
 {
     [SerializeField] Rigidbody rb;
+    [SerializeField] float gizmoRadius;
 
     //movement
-    [SerializeField] float moveForce;
+    [SerializeField] float xForce;
+    [SerializeField] float zForce;
     [SerializeField] Transform centerOfMovement;
-    private Vector3 directionTowardsCenter;
+    private Vector3 moveDirection;
+    [SerializeField] float xBound;
+    [SerializeField] float zBound;
 
     //aggro
     [SerializeField] float aggroRadius;
@@ -24,15 +28,49 @@ public class BallMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        directionTowardsCenter = (centerOfMovement.position - transform.position).normalized;
-        print(directionTowardsCenter);
-        rb.AddForce(directionTowardsCenter * moveForce, ForceMode.Impulse);
+        CheckBounds();
 
-        
+        moveDirection = new Vector3(xForce, 0, zForce);
+
+        rb.AddForce(moveDirection, ForceMode.Impulse);
     }
 
-    void FindPerpendicular()
+    void CheckBounds()
     {
-        
+        if(transform.position.x >= centerOfMovement.position.x + xBound) {
+            //set x dir neg
+            if(xForce > 0) {
+                xForce *= -1;
+            }
+        }
+        if(transform.position.x <= centerOfMovement.position.x - xBound) {
+            //set x dir pos
+            if(xForce < 0) {
+                xForce *= -1;
+            }
+        }
+
+        if(transform.position.z >= centerOfMovement.position.z + zBound) {
+            //set z dir neg
+            if(zForce > 0) {
+                zForce *= -1;
+            }
+        }
+        if(transform.position.z <= centerOfMovement.position.z - zBound) {
+            //set z dir pos
+            if(zForce < 0) {
+                zForce *= -1;
+            }
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+
+        Gizmos.DrawWireSphere(new Vector3(centerOfMovement.position.x + xBound, centerOfMovement.position.y, centerOfMovement.position.z), gizmoRadius);
+        Gizmos.DrawWireSphere(new Vector3(centerOfMovement.position.x - xBound, centerOfMovement.position.y, centerOfMovement.position.z), gizmoRadius);
+        Gizmos.DrawWireSphere(new Vector3(centerOfMovement.position.x, centerOfMovement.position.y, centerOfMovement.position.z + zBound), gizmoRadius);
+        Gizmos.DrawWireSphere(new Vector3(centerOfMovement.position.x, centerOfMovement.position.y, centerOfMovement.position.z - zBound), gizmoRadius);
     }
 }
