8ccd6cb6ab67531cd0b63348f0d3faadc6303eb0
Michael-1898 <85533503+Michael-1898@users.noreply.github.com>
2023-07-17 11:44:16 -0400

    timer ui

--- MarbleMadness_NHSGA/Assets/Scripts/UI/UITimer.cs
+++ MarbleMadness_NHSGA/Assets/Scripts/UI/UITimer.cs
@@ -7,20 +7,76 @@ using TMPro;
 public class UITimer : MonoBehaviour
 {
     [SerializeField] GameObject timerTxt;
+    [SerializeField] GameObject timeDisclaimer;
+    [SerializeField] float width;
 
     private float timer;
+    [SerializeField] float disclaimerDuration;
+    private bool disclaimerDone = false;
+    private bool timerFull = false;
+
+    [SerializeField] float levelTime;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
+        DisplayDisclaimer();
 
-        timerTxt.GetComponent<TMP_Text>().text = "Timer: " + Mathf.Round(timer * 100f) / 100f;
+        if(disclaimerDone && !timerFull) {
+            FillUpTimer();
+        }
+
+        if(timerFull) {
+            timeDisclaimer.SetActive(false);
+
+            DecreaseTimer();
+        }
+    }
+
+    public void SetTimer(float time)
+    {
+        timer = time;
+    }
+
+    void DisplayDisclaimer()
+    {
+        if(disclaimerDuration > 0) {
+            disclaimerDuration -= Time.deltaTime;
+        }
+        
+        if(disclaimerDuration <= 0 && !disclaimerDone) {
+            disclaimerDone = true;
+        }
+    }
+
+    void FillUpTimer()
+    {
+        //increase timer
+        timer += 25 * Time.deltaTime;
+        timerTxt.GetComponent<TMP_Text>().text = "" + Mathf.Round(timer * 100f) / 100f;
+        //timerTxt.SetText($"<mspace={width}em{Mathf.Round(timer * 100f) / 100f}");
+        if(timer >= 60) {
+            timerTxt.GetComponent<TMP_Text>().text = "60";
+            timerFull = true;
+        }
+
+        //decrease disclaimer time
+        if(levelTime - timer < 0) {
+            timeDisclaimer.GetComponent<TMP_Text>().text = "Time to finish race: 0";
+        } else {
+            timeDisclaimer.GetComponent<TMP_Text>().text = "Time to finish race: " + Mathf.Round((levelTime - timer) * 100f) / 100f;
+        }
+    }
+
+    void DecreaseTimer()
+    {
+        timer -= Time.deltaTime;
+        timerTxt.GetComponent<TMP_Text>().text = "" + Mathf.Round(timer * 100f) / 100f;
     }
 }
