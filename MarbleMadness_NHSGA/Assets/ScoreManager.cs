f901315818e2cfd9fe32e0d614a0fa9ada138927
wally <69739715+wallysaurus@users.noreply.github.com>
2023-07-19 13:35:01 -0400

    I did stuff. i dunno what that stuff is.

--- MarbleMadness_NHSGA/Assets/ScoreManager.cs
+++ MarbleMadness_NHSGA/Assets/ScoreManager.cs
@@ -11,8 +11,8 @@ public class ScoreManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpeedScore", 0, 1);
         player = gameObject.GetComponent<PlayerMove>().gameObject;
+        InvokeRepeating("SpeedScore", 0, 1);
     }
 
     // Update is called once per frame
