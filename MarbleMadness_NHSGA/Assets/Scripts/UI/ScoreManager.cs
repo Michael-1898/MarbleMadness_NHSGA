021bd38cf873d4ce9b45ba672743cfdec8effc3c
Michael-1898 <85533503+Michael-1898@users.noreply.github.com>
2023-07-19 15:19:38 -0400

    score manager

--- MarbleMadness_NHSGA/Assets/Scripts/UI/ScoreManager.cs
+++ MarbleMadness_NHSGA/Assets/Scripts/UI/ScoreManager.cs
@@ -11,7 +11,7 @@ public class ScoreManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        player = gameObject.GetComponent<PlayerMove>().gameObject;
+        player = GameObject.FindWithTag("Player");
         InvokeRepeating("SpeedScore", 0, 1);
     }
 
